9f11b567f6eaa52452b2af71057fe9d89129f5f5
GeumBin <lgb9811@gmail.com>
2022-11-04 23:02:29 +0900

    📦Chore: 경로 설정 #7
    아직 html 파일이 없는 경로와 관련한 html 이름은 임의로 설정해 두었습니다.
    영화 페이지에서 하드코딩했던 태그 삭제

---
+++ javascript/path.js
@@ -0,0 +1,65 @@
+// 회원가입 페이지로 이동
+function to_signup() {
+    location.href ="signup.html"
+}
+
+// 로그인 페이지로 이동
+function to_signin() {
+    location.href = "signin.html"
+}
+
+// 영화 전체 리스트 페이지로 이동
+function to_movie_list() {
+    location.href = "movie_list.html"
+}
+
+// 영화 상세 페이지로 이동
+function to_movie_detail() {
+    location.href = "movie_detail.html"
+}
+
+// 게시글 전체 리스트 페이지로 이동
+function to_articles() {
+    location.href = "articles.html"
+}
+
+// 게시글 상세 페이지로 이동
+function to_article_detail() {
+    location.href = "article_detail.html"
+}
+
+// 게시글 입력 페이지로 이동
+function to_write_article() {
+    location.href = "write_article.html"
+}
+
+// 프로필 페이지로 이동
+function to_profile() {
+    location.href = "profile.html"
+}
+
+// MBTI 설명 페이지로 이동
+// 16개의 mbti
+function to_mbti_detail() {
+    location.href = "mbti_detail.html"
+}
+
+// 프로필 입력창으로 이동, 생성 및 수정
+function to_insert_profile() {
+    location.href = "insert_profile.html"
+}
+
+// 친구 추천 페이지로 이동
+function to_friends() {
+    location.href = "friends.html"
+}
+
+// mbti 버튼 누르면 해당 mbti를 가진 작성자들이 쓴 게시글 리스트만 뜨는 페이지
+function to_mbti_menu() {
+    location.href = "mbti_menu.html"
+}
+
+// 추천된 영화 리스트 페이지로 이동
+function to_recommended_movies() {
+    location.href = "recommended_movies.html"
+}

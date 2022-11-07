df273357266fddce8cbda2de39b07cd525fd1b6c
kmg0485 <kmg0485@naver.com>
2022-11-07 18:00:13 +0900

    :lipstick: Design : profile.html and css, javascript

--- javascript/movie_list.js
+++ javascript/movie_list.js
@@ -1,42 +1,39 @@
 window.onload = () => {
-    handleAuthorization()
+  handleAuthorization();
+};
+
+async function handleAuthorization() {
+  const payload = localStorage.getItem("payload");
+  const payload_parse = JSON.parse(payload);
+  const nickname = payload_parse.nickname;
+  const greeting = document.getElementById("greeting");
+  greeting.innerText = `${nickname}님, 반가워요!`;
+
+  const response = await fetch("http://127.0.0.1:8000/movies/", {
+    headers: {
+      authorization: "Bearer " + localStorage.getItem("access"),
+    },
+    method: "GET",
+  });
+
+  response_json = await response.json();
+  console.log(response_json);
+  const movies = document.getElementById("container");
+
+  response_json.forEach((element) => {
+    console.log(element.poster);
+    const newMovieLink = document.createElement("a");
+    newMovieLink.href = "#";
+    movies.appendChild(newMovieLink);
+    const newMovie = document.createElement("img");
+    newMovie.src = "/Users/lgb/Desktop/ABBBA_MBTI" + element.poster;
+    newMovieLink.appendChild(newMovie);
+  });
 }
 
-
-async function handleAuthorization(){
-    const payload = localStorage.getItem("payload");
-    const payload_parse = JSON.parse(payload)
-    const nickname = payload_parse.nickname
-    const greeting = document.getElementById("greeting")
-    greeting.innerText = `${nickname}님, 반가워요!`
-
-    const response = await fetch('http://127.0.0.1:8000/movies/',{
-        headers : {
-            "authorization" : "Bearer " + localStorage.getItem("access")
-        },
-        method : 'GET'
-    })
-
-    response_json = await response.json()
-    console.log(response_json)
-    const movies = document.getElementById("container")
-
-    response_json.forEach(element => {
-        console.log(element.poster)
-        const newMovieLink = document.createElement("a")
-        newMovieLink.href = "#"
-        movies.appendChild(newMovieLink)
-        const newMovie = document.createElement("img")
-        newMovie.src = "/Users/lgb/Desktop/ABBBA_MBTI"+element.poster
-        newMovieLink.appendChild(newMovie)
-
-
-    })
-}
-
-function handleLogout(){
-    localStorage.removeItem("access")
-    localStorage.removeItem("refresh")
-    localStorage.removeItem("payload")
-    alert("로그아웃이 완료되었습니다.")
+function handleLogout() {
+  localStorage.removeItem("access");
+  localStorage.removeItem("refresh");
+  localStorage.removeItem("payload");
+  alert("로그아웃이 완료되었습니다.");
 }
